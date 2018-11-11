8ff7c475505dc316eb93c696e08421229acfd0de
kayosan <tokyo0422xoxo88@gmail.com>
2018-11-11 12:45:16 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 'use strict';
+const memo = new Map(); // 連想配列Mapの定義
+memo.set(0,0); // ０と１は答えが決まっている
+memo.set(1,1); // set(番号,答え)
+
 function fib(n) {
-    if (n === 0) {
-        return 0;
-    }else if(n == 1){
-        return 1;
+    if (memo.has(n)) { // has(n)→nをキーとした答えをもっている
+        return memo.get(n); // 0番目と1番目のとき
     }
-    return fib(n -1) + fib(n-2);
+    const value = fib(n -1) + fib(n - 2);
+    memo.set(n,value);
+    return value;
 }
 
 const length = 40;
